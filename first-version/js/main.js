907a5134a40e62a69fc37a8609f1c1d1f9c0d4f4
Francesco <francesco.ferruccio88@gmail.com>
2020-06-25 16:13:41 +0200

    riorganizzato progetto per portfolio

--- first-version/js/main.js
+++ first-version/js/main.js
@@ -1,13 +1,8 @@
 $(document).ready(function() {
   // handlebars
-  var movieSource = $("#movie-template").html();
-  var movieTemplate = Handlebars.compile(movieSource);
+  var source = $("#movie-template").html();
+  var template = Handlebars.compile(source);
 
-  var staffSource = $("#staff-template").html();
-  var staffTemplate = Handlebars.compile(staffSource);
-
-  var selectSource = $("#select-template").html();
-  var selectTemplate = Handlebars.compile(selectSource);
   // variabili globali
   var searchBtn = $("#search-btn");
   var contRisultati = $(".result-container");
@@ -46,31 +41,6 @@ $(document).ready(function() {
     }
   })
 
-  // al click su una qualsiasi card apro la rispettiva modale
-  $(".result-container").on("click", ".movie-card", function() {
-    var id=$(this).data("id");
-    $(this).find(".modale").toggle();
-
-    var type = $(this).data("type");
-
-    // cerco attori e regista del film cliccato
-    cercaStaff(id, type);
-  });
-
-  // funzione filtro tramite select
-  $("#genre-select").change(function() {
-    var filtro = $(this).val();
-    $(".movie-card").each(function() {
-      var thisGeneri = "" + $(this).data("genere");
-      var arrayGeneri = thisGeneri.split(",");
-      if(arrayGeneri.includes(filtro)) {
-        $(this).show();
-      } else {
-        $(this).hide();
-      }
-    }
-  )
-  });
 
   // DICHIARAZIONE FUNZIONI ----------------------------------------------------
 
@@ -90,6 +60,7 @@ $(document).ready(function() {
       url: endPoint,
       method: "GET",
       data: {
+        url: endPoint,
         api_key: "db123098e9fe123d1b0a79cc401c920d",
         query: query,
         language: "it-IT"
@@ -97,7 +68,6 @@ $(document).ready(function() {
       success: function(data, stato) {
         // salvo l'array di oggetti
         listaRisultati = data.results;
-        getGenreList(tipo);
         stampaRisultati(listaRisultati, tipo);
       },
       error: function(richiesta, stato, errore) {
@@ -108,7 +78,7 @@ $(document).ready(function() {
 
   // funzione output risultato ricerca
   function stampaRisultati(arrayOggetti, tipo) {
-    var title, origTitle, release, type;
+    var title, origTitle;
 
     if(arrayOggetti.length == 0) {
       if(tipo == "Film") {
@@ -121,34 +91,22 @@ $(document).ready(function() {
         if (tipo == "Film") {
           title = arrayOggetti[i].title;
           origTitle = arrayOggetti[i].original_title;
-          release = arrayOggetti[i].release_date;
-          type = "movie";
         } else {
           title = arrayOggetti[i].name;
           origTitle = arrayOggetti[i].original_name;
-          release = arrayOggetti[i].first_air_date;
-          type = "tv";
         }
 
         var context = {
-          id: arrayOggetti[i].id,
-          type: type,
-          genre: arrayOggetti[i].genre_ids,
           poster: generatePoster(arrayOggetti[i].poster_path),
           titolo: title,
           titoloOriginale: origTitle,
           lingua: generateFlag(arrayOggetti[i].original_language),
           voto: votoInStelline(arrayOggetti[i].vote_average),
-          anno: generateAnno(release),
           tipo: tipo,
-          trama: generateOverview(arrayOggetti[i].overview, "riassunto"),
-          tramaCompleta: generateOverview(arrayOggetti[i].overview, "completa"),
-          backdrop: genrateBackdrop(arrayOggetti[i].backdrop_path)
+          trama: generateOverview(arrayOggetti[i].overview)
         };
 
-        $(".top-result").addClass("visible");
-
-        var html = movieTemplate(context);
+        var html = template(context);
         contRisultati.append(html);
 
         // controllo se il titolo è uguale al titolo originale nascondo
@@ -215,7 +173,7 @@ $(document).ready(function() {
   }
 
   // funzione output trama
-  function generateOverview(trama, tipo) {
+  function generateOverview(trama) {
     var result;
 
     if(trama === "") {
@@ -225,127 +183,10 @@ $(document).ready(function() {
     }
 
     // se la descrizione è troppo lunga la riduco a 300 caratteri
-    if(result.length > 300 && tipo === "riassunto") {
+    if(result.length > 300) {
       result = trama.slice(0, 300) + "... (continua)";
     }
 
     return result;
-  }
-
-  // funzione output sfondo modale
-  function genrateBackdrop(sfondo) {
-    var result;
-
-    if (sfondo == null) {
-      result = "";
-    } else {
-      result = '<img src="https://image.tmdb.org/t/p/w1280' + sfondo + '">';
-    }
-
-    return result;
-  }
-
-  // funzione output anno
-  function generateAnno(releaseDate) {
-    var anno;
-
-    if (releaseDate == undefined) {
-      anno = "Dato non disponibile";
-    } else {
-      anno = releaseDate.slice(0, 4);
-    }
-
-    return anno;
-  }
-
-  // funzione output attori
-  function cercaStaff(movieID, tipo) {
-    $(".credits").html("");
-
-    $.ajax({
-      url: 'https://api.themoviedb.org/3/' + tipo + '/' + movieID + '/credits',
-      data: {
-        api_key: "db123098e9fe123d1b0a79cc401c920d",
-      },
-      method: "GET",
-      success: function (data, stato) {
-        var movie = data.cast;
-
-        // salvo i primi 9 risultati
-        for(var i = 0; i < 9; i++) {
-          var personaggio = movie[i].character;
-          var foto = movie[i].profile_path;
-
-          if(personaggio === "") {
-            personaggio = "non disponibile"
-          }
-
-          if(foto === null) {
-            foto = 'img/attore.jpg';
-          } else {
-            foto = 'https://image.tmdb.org/t/p/w185' + movie[i].profile_path;
-          }
-
-          var context = {
-            staffImg: foto,
-            staffName: movie[i].name,
-            staffCharacter: personaggio
-          };
-          var html = staffTemplate(context);
-
-          $(".credits").append(html);
-        }
-      },
-      error: function (richiesta, stato, errore) {
-        console.log("ERRORE", richiesta, stato, errore);
-      }
-    }
-    );
-  }
-
-  // funzione che richiede la lista dei generi all'api
-  function getGenreList(tipo) {
-    var endPoint;
-
-    if(tipo === "Film") {
-      endPoint = "https://api.themoviedb.org/3/genre/movie/list";
-    } else if (tipo === "TV") {
-      endPoint = "https://api.themoviedb.org/3/genre/tv/list";
-    }
-
-    $.ajax({
-      url: endPoint,
-      data: {
-        api_key: "db123098e9fe123d1b0a79cc401c920d",
-        language: "it-IT"
-      },
-      method: "GET",
-      success: function(data, stato) {
-        var listGenre = [];
-        for (var i = 0; i < data.genres.length; i++) {
-          listGenre.push(data.genres[i]);
-        };
-
-        createSelect(listGenre);
-      },
-      error: function(richiesta, stato, errore) {
-        console.log("ERRORE");
-      }
-    });
-  }
-
-  // funzione che inserisce la lista dei generi restituita dall'api nella select
-  function createSelect(generi) {
-
-    for(var i=0; i < generi.length; i++) {
-      var context = {
-        idGenre: generi[i].id,
-        nameGenre: generi[i].name
-      };
-
-      var html = selectTemplate(context);
-
-      $("#genre-select").append(html);
-    }
   }
 });
